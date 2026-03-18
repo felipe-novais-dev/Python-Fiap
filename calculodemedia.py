edf4fee0ebb77d0be2267882b8c4e2a4df5dd063
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 11:28:55 -0300

    Adicionando calculodemedia2

--- calculodemedia.py
+++ calculodemedia.py
@@ -13,3 +13,14 @@ n3 = float(input('Nota 3: '))
 media = (n1+n2+n3)/3
 #Saída de Dados
 print('Media: ',)
+
+
+'''Solicitar o nome do aluno
+- Solicitar o número de faltas
+- Adicionar a condiçao do aluno realizar o exame
+- Aprovaçao - media>=6 e numero de faltas <= 18
+- Reprovado - numero de faltas <= 18
+Examente - se a mediai for <= 6 e numero >=18
+-Obter uma nova nota 
+- Calcular uma nova media
+-Verificaçao se aprovado ou reprovado'''
---
+++ calculodemedia2.py
@@ -0,0 +1,9 @@
+'''Solicitar o nome do aluno
+- Solicitar o número de faltas
+- Adicionar a condiçao do aluno realizar o exame
+- Aprovaçao - media>=6 e numero de faltas <= 18
+- Reprovado - numero de faltas <= 18
+Examente - se a mediai for <= 6 e numero >=18
+-Obter uma nova nota 
+- Calcular uma nova media
+-Verificaçao se aprovado ou reprovado'''
