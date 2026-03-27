f630af5f60f795d911ff806db15f95f07c107bb5
Felipe Novais de Sousa <fefenovaissp@gmail.com>
2026-03-27 08:00:47 -0300

    Add age classification program in Python
    Implement age classification based on user input.

--- exercicioidade.py
+++ exercicioidade.py
@@ -1,1 +1,30 @@
+##Exercício 2 – Classificação de Idade
+ 
+#Escreva um programa em Python que solicite ao usuário a entrada de uma idade (número inteiro).
+#O programa deve classificar a idade informada de acordo com as regras abaixo:
+#0 a 12 → "Criança"
+#13 a 17 → "Adolescente"
+#18 a 59 → "Adulto"
+#60 ou mais → "Idoso"
+#Idade negativa → "Idade inválida"
+#Requisitos:
+#Utilizar apenas if, elif e else.
+##Validar valores negativos.
+#Exemplo:
+##Digite a idade: 25
+##Adulto
 
+print('--Idade--')
+idade = int(input('Idade: '))
+
+if idade < 0:
+    print('Idade inválida')
+elif idade <= 12:
+    print('Criança')
+elif idade <= 17:
+    print('Adolescente')
+elif idade <= 59:
+    print('Adulto')
+else:  # idade >= 60
+    print('Idoso')
+                
